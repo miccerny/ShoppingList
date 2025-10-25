42f57e492585af9d67563fee2e27e992c9e3c2d6
Michal Černý <cerny.michal93@gmail.com>
2025-10-25 22:36:43 +0200

    update

--- client/src/Lists/ListTable.jsx
+++ client/src/Lists/ListTable.jsx
@@ -9,28 +9,30 @@ const ListTable = ({ label, items, onEdit, onDelete }) => {
 
             <div className="d-flex flex-wrap gap-3">
                 {items.map((list) =>
-                    <div key={list._id || list.id} className="card" style={{ width: "18rem" }}>
-                        <div className="card-body">
+                    <div key={list._id || list.id} className="card w-100">
+                        <div className="card-body d-flex justify-content-between align-items-center">
                             <h5 className="card-title">
                                 {list.name}
                             </h5>
+                            <div className="d-flex flex-column  align-items-end">
                             <div className="btn-group">
                                 <Link to={`/list/show/${list._id || list.id}`}>
                                     Zobrazit
                                 </Link>
                             </div>
                             <button
-                                className="btn btn-sm btn-primary me-2"
+                                className="btn btn-sm btn-primary mt-2"
                                 onClick={() => onEdit(list._id || list.id)}
                             >
                                 Upravit
                             </button>
                             <button
-                                className="btn btn-sm btn-danger"
+                                className="btn btn-sm btn-danger mt-2"
                                 onClick={() => onDelete(list._id || list.id)}
                             >
                                 Smazat
                             </button>
+                            </div>
                         </div>
                     </div>
                 )}
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: 5173, // 👈 tady nastavíš port napevno
+    port: 517, // 👈 tady nastavíš port napevno
   },
 })
