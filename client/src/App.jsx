d26f038aad6db76d38dbcc54377516228adf47cf
Michal Černý <cerny.michal93@gmail.com>
2026-01-13 11:18:21 +0100

    update - refactoring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ export function App() {
    * - flash: currently displayed message (if any)
    * - clear: function to remove the message
    */
-  const { flash, clear } = useFlash();
+  const { flash} = useFlash();
 
   /**
    * Authentication session state.
